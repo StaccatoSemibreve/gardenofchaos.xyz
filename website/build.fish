2a71111a697cfd37a7728f734cf63be28c9afff0
janet <37193001+StaccatoSemibreve@users.noreply.github.com>
2020-09-16 12:13:10 +0100

    changed the build method

--- website/build.fish
+++ website/build.fish
@@ -1,15 +1,20 @@
 function build
 	echo "<!DOCTYPE html>
 <html lang=\"\">
-	<head>"
-	cat $argv[1]/head.html	
+	<head>
+		<meta charset=\"utf-8\">
+		<title>$argv[2]</title>
+		<link rel=\"icon\" type=\"image/png\" href=\"FSgardenOfChao-196x196.png\" sizes=\"196x196\">
+		<link rel=\"stylesheet\" href=\"navbar-style.css\">"
+	for i in $argv[3..-1]
+		echo $i
+	end
 	echo "		<style>"
 	cat $argv[1]/style.css
-	cat navbar/style.css
 	echo "		</style>
 	</head>
 	<body>"
-	cat navbar/main.html
+	cat navbar.html
 	cat $argv[1]/main.html
 	echo "		<footer></footer>
 	</body>
@@ -22,15 +27,17 @@ end
 function build-static
 	echo "<!DOCTYPE html>
 <html lang=\"\">
-	<head>"
-	cat $argv[1]/head.html	
-	echo "		<style>"
+	<head>
+		<meta charset=\"utf-8\">
+		<title>$argv[2]</title>
+		<link rel=\"icon\" type=\"image/png\" href=\"FSgardenOfChao-196x196.png\" sizes=\"196x196\">
+		<link rel=\"stylesheet\" href=\"navbar-style.css\">
+		<style>"
 	cat $argv[1]/style.css
-	cat navbar/style.css
 	echo "		</style>
 	</head>
 	<body>"
-	cat navbar/main.html
+	cat navbar.html
 	cat $argv[1]/main.html
 	echo "		<footer></footer>
 	</body>
@@ -40,18 +47,18 @@ end
 function build-markdown
 	echo "<!DOCTYPE html>
 <html lang=\"\">
-	<head>"
-	cat $argv[1]/head.html	
-	echo "		<style>"
-	cat $argv[1]/style.css
-	cat navbar/style.css
-	echo "		</style>
+	<head>
+		<meta charset=\"utf-8\">
+		<title>$argv[2]</title>
+		<link rel=\"icon\" type=\"image/png\" href=\"FSgardenOfChao-196x196.png\" sizes=\"196x196\">
+		<link rel=\"stylesheet\" href=\"markdown-style.css\">
+		<link rel=\"stylesheet\" href=\"navbar-style.css\">
 	</head>
 	<body>"
-	cat navbar/main.html
+	cat navbar.html
 	echo "	    <main>
 			<div id=\"center-div\">"
-	markdown $argv[1]/main.md | sed 's/^/\t\t\t\t/'
+	markdown -f "toc" $argv[1].md | sed 's/^/\t\t\t\t/'
 	echo "		</div>
     </main>
 	<footer></footer>
@@ -60,21 +67,22 @@ function build-markdown
 end
 
 
-build-static index					> ../index.html
-build-static rules					> ../rules.html
-build-static styles					> ../styles.html
-build-static rulesets				> ../rulesets.html
-build-static resources				> ../resources.html
-build-static commentators			> ../commentators.html
-build tournaments					> ../tournaments.html
-build tournaments-tpkweekly 		> ../tournaments-tpkweekly.html
-build random						> ../random.html
-build teams							> ../teams.html
-build teams2						> ../teams2.html
-build teamsfancy					> ../teamsfancy.html
-build teamdisplay					> ../teamdisplay.html
-build-markdown thoughts				> ../thoughts.html
-build-markdown thoughts-throws		> ../thoughts-throws.html
-build-markdown thoughts-dice		> ../thoughts-dice.html
-build-markdown thoughts-advantage	> ../thoughts-advantage.html
-build-markdown thoughts-lumoki		> ../thoughts-lumoki.html
+build-markdown index "Garden of Chaos: Home"				> ../index.html
+build-markdown rules "Garden of Chaos: Rules"				> ../rules.html
+build-markdown styles "Garden of Chaos: Tournament Formats"	> ../styles.html
+build-markdown rulesets "Garden of Chaos: Rulesets"			> ../rulesets.html
+build-markdown resources "Garden of Chaos: Resources"		> ../resources.html
+build-markdown commentators "Garden of Chaos: Commentators"	> ../commentators.html
+build tournaments "Garden of Chaos: Tournament Archive"		> ../tournaments.html
+build random "Garden of Chaos: Randomiser"					> ../random.html
+
+build-markdown thoughts "Janet's Thoughts on Stuff™"				> ../thoughts.html
+build-markdown thoughts-throws "Janet's Thoughts on Throws"			> ../thoughts-throws.html
+build-markdown thoughts-dice "Janet's Thoughts on Dice"				> ../thoughts-dice.html
+build-markdown thoughts-advantage "Janet's Thoughts on Advantage"	> ../thoughts-advantage.html
+build-markdown thoughts-lumoki "Janet's Thoughts on Lum's Oki"		> ../thoughts-lumoki.html
+
+build teams "Garden of Chaos: Team Battle Tool"						> ../teams.html
+build teams2 "Garden of Chaos: Async Teams Tool"					> ../teams2.html
+build teamsfancy "Garden of Chaos: Team Selector" (for i in "grave" "jaina" "geiger" "argagarg" "setsuki" "valerie" "rook" "midori" "lum" "degrey" "quince" "onimaru"; echo "		<link rel=\"preload\" href=\"media/$i.png\" as=\"image\" type=\"image/png\">"; end)	> ../teamsfancy.html
+build teamdisplay "Garden of Chaos: Team Battle" (for i in "grave" "jaina" "geiger" "argagarg" "setsuki" "valerie" "rook" "midori" "lum" "degrey" "quince" "onimaru"; echo "		<link rel=\"preload\" href=\"media/$i.png\" as=\"image\" type=\"image/png\">"; end)	> ../teamdisplay.html
