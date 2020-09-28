9e44c7a00620d3bfa0474e0683b6c1b1673ab565
janet <37193001+StaccatoSemibreve@users.noreply.github.com>
2020-09-28 17:26:57 +0100

    team battle tool!!!!!

---
+++ editable/non-static/teambattle/script.js
@@ -0,0 +1,159 @@
+var i = 0;
+var params = (new URL(document.location)).searchParams;
+
+var teams = [[],[]];
+if (params.get("team1"))
+	teams[0] = params.get("team1").split(",").map(function(e, i) {return [e, i+1];});
+if (params.get("team2"))
+	teams[1] = params.get("team2").split(",").map(function(e, i) {return [e, i+1];});
+console.log("team 1: " + teams[0]);
+console.log("team 2: " + teams[1]);
+
+var seed = 13645143;
+if (params.get("seed"))
+	seed = +params.get("seed");
+console.log("seed = " + seed);
+
+teams.forEach(function(chars, team) {
+	chars.forEach(function(char, index) {
+		charIcon = document.getElementById(char[0]).cloneNode();
+		charIcon.setAttribute("id", `team${team+1}member${index}`);
+		charIcon.setAttribute("style", `width:${100/chars.length}%;`);
+		document.getElementById(`team${team+1}`).appendChild(charIcon);
+	});
+});
+
+games = [shufflify(teams[0], teams[1].length), shufflify(teams[1], teams[0].length)]
+console.log(games);
+
+function seededRandom(min, max) {
+max = max || 1;
+min = min || 0;
+
+seed = (seed * 9301 + 49297) % 233280;
+var rnd = seed / 233280;
+
+return min + rnd * (max - min);
+}
+function shuffle(a) {
+    var j, x, i;
+    for (i = a.length - 1; i > 0; i--) {
+        j = Math.floor(seededRandom(0,i+1));
+        x = a[i];
+        a[i] = a[j];
+        a[j] = x;
+    }
+    return a;
+}
+function shufflify(team, size) {
+    var result = [];
+    while(result.length < team.length*size)
+        result = result.concat(shuffle(team.slice(0)));
+	return result;
+}
+
+function createCharElement(char, teamNumber) {
+    i++;
+    
+    div = document.createElement("DIV");
+    div.setAttribute("class","icon");
+	div.setAttribute("id", "team"+teamNumber+"charicon");
+    
+    clicker = document.createElement("A");
+    clicker.setAttribute("href", "javascript:void(0);");
+    clicker.setAttribute("title", "Team " + teamNumber + "'s " + char + " wins!");
+    clicker.setAttribute("onclick", 'winGame("' + teamNumber+'")');
+    div.appendChild(clicker);
+	
+    clicker.appendChild(document.getElementById(char).cloneNode());
+    clicker.appendChild(document.getElementById("check").cloneNode());
+    
+    return div;
+}
+
+function startMatch() {
+	teams.forEach(function(team, teamNumber) {
+		team.forEach(function(char, index) {
+			document.getElementById("team"+(teamNumber+1)+"member"+index).setAttribute("style", `width:${100/team.length}%; opacity: 50%;`);
+		});
+	});
+	
+	nextGame();
+}
+
+function nextGame() {
+	chars = [games[0][0], games[1][0]];
+	console.log(chars.join(" vs "));
+	teams.forEach(function(team, teamNumber) {
+		team.forEach(function(char, index) {
+			if(index == chars[teamNumber][1]-1)
+				document.getElementById("team"+(teamNumber+1)+"member"+index).setAttribute("style", `width:${100/team.length}%;`);
+		});
+	});
+	document.getElementById("team1char").innerHTML = "";
+	document.getElementById("team1char").appendChild(createCharElement(chars[0][0], 1));
+	document.getElementById("team2char").innerHTML = "";
+	document.getElementById("team2char").appendChild(createCharElement(chars[1][0], 2));
+}
+
+function winGame(winningTeam) {
+	archiveGame(winningTeam);
+	teams.forEach(function(team, teamNumber) {
+		if(teamNumber+1 == winningTeam) {
+			document.getElementById("team"+(teamNumber+1)+"member"+(games[teamNumber][0][1]-1)).setAttribute("style", `width:${100/team.length}%; opacity: 25%;`);
+			games[teamNumber] = games[teamNumber].filter(char => char[1] != games[teamNumber][0][1]);
+		}
+		else {
+			document.getElementById("team"+(teamNumber+1)+"member"+(games[teamNumber][0][1]-1)).setAttribute("style", `width:${100/team.length}%; opacity: 50%;`);
+			games[teamNumber] = games[teamNumber].splice(1);
+		}
+	});
+	console.log(games);
+	
+	if(games[0].length == 0)
+		winMatch(1);
+	else if(games[1].length == 0)
+		winMatch(2);
+	else
+		nextGame();
+}
+
+function archiveGame(winningTeam) {
+	var sign1;
+	var sign2;
+	if (winningTeam == 1) {
+		sign1 = document.getElementById("check-sign").cloneNode();
+		sign2 = document.getElementById("x-sign").cloneNode();
+	}
+	else {
+		sign1 = document.getElementById("x-sign").cloneNode();
+		sign2 = document.getElementById("check-sign").cloneNode();
+	}
+	char1 = document.getElementById(games[0][0][0]).cloneNode();
+	char2 = document.getElementById(games[1][0][0]).cloneNode();
+	
+	vs = document.createElement("DIV");//<div id="vsdiv" class="vs"><h2 id="vs" class="vs">vs</h2></div>
+	vs.setAttribute("id", "vsdiv");
+	vs.setAttribute("class", "vs");
+	vstext = document.createElement("H2");
+	vstext.setAttribute("id", "vs");
+	vstext.setAttribute("class", "vs");
+	vstext.innerHTML = "vs";
+	vs.appendChild(vstext);
+	
+	div = document.createElement("DIV");
+	div.setAttribute("class", "archive");
+	div.appendChild(sign1);
+	div.appendChild(char1);
+	div.appendChild(vs);
+	div.appendChild(char2);
+	div.appendChild(sign2);
+	
+	document.getElementById("archive").appendChild(div);
+}
+
+function winMatch(winningTeam) {
+	console.log(`Team ${winningTeam} wins!`);
+	document.getElementById("versus").innerHTML=`<h2 class="title" style="grid-area: all;">Team ${winningTeam} wins!!!</h2>`;
+	document.getElementById("versus").setAttribute("id", "versuswin");
+}
--- editable/non-static/teamselector/script.js
+++ editable/non-static/teamselector/script.js
@@ -1,20 +1,26 @@
 var teams = [[],[]];
 var i = 0;
 
+function updateTeams() {
+	if(teams[0].length > 0 && teams[1].length > 0)
+		document.getElementById("submitlink").href = "teamdisplay?seed=" + (10000+Math.floor(Math.random()*90000)) + "&team1="+teams[0].filter(n=>n!==undefined).join() + "&team2="+teams[1].filter(n=>n!==undefined).join();
+	else
+		document.getElementById("submitlink").href = "javascript:void(0);";
+}
 function createCharElement(charName, teamNumber) {
     i++;
     
     div = document.createElement("DIV");
-    div.setAttribute("id",i+"char");
-    div.setAttribute("class","icon;");
+    div.setAttribute("id",teamNumber+""+teams[teamNumber-1].length);
+    div.setAttribute("class","icon");
     
     clicker = document.createElement("A");
     clicker.setAttribute("href", "javascript:void(0);");
     clicker.setAttribute("title", "Remove this "+charName+" from Team "+teamNumber+".");
-    clicker.setAttribute("onclick", 'removeChar("'+i+'char", '+teamNumber+')');
+    clicker.setAttribute("onclick", 'removeChar("'+teamNumber+teams[teamNumber-1].length+'", '+teamNumber+')');
     clicker.setAttribute("id", i+"char");
     div.appendChild(clicker);
-    clicker.appendChild(document.getElementById(charName.toLowerCase()).cloneNode());
+    clicker.appendChild(document.getElementById(charName).cloneNode());
     clicker.appendChild(document.getElementById("minus").cloneNode());
     
     return div;
@@ -24,18 +30,20 @@ function addChar(char, teamNumber) {
     teams[teamNumber-1] = teams[teamNumber-1].concat(char);
     
     document.getElementById('team'+teamNumber+'div')
-            .appendChild(createCharElement(char, 1));
+            .appendChild(createCharElement(char, teamNumber));
     
-    if(teams[0].length > 0 && teams[1].length > 0)
-        document.getElementById("submitlink").href = "https://gardenofchaos.xyz/teamdisplay?seed=" + (10000+Math.floor(Math.random()*90000)) + "&team1="+teams[0].join() + "&team2="+teams[1].join();
+    updateTeams();
 }
 function removeChar(char, teamNumber) {
     console.log("Remove char "+char+" from team "+teamNumber+".");
-    teams[teamNumber-1].splice(0+char.substr(0,1),1);
+    delete teams[teamNumber-1][+char.substring(1)-1]
     document.getElementById(char).remove();
+    
+    updateTeams();
 }
 function clearTeam(teamNumber) {
     console.log("Clear team "+teamNumber+".");
     teams[teamNumber-1]=[];
     document.getElementById("team"+teamNumber+"div").innerHTML="";
+	updateTeams();
 }
